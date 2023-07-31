d6d4f554dcb18aa083c0060247563ef46e3fbe65
Ivan Kostiuchkov (a.k.a. i-401) <sveter2001@mail.ru>
2023-07-31 21:24:31 +0300

    grow

--- Assets/Code/RaisingFood.cs
+++ Assets/Code/RaisingFood.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+    //[SerializeField] private Body[] myBody = new Body[999];
+    [SerializeField] public Sprite sprite11;
+    [SerializeField] public Sprite sprite22;
+    private GameObject[] myBody = new GameObject[999];
+
     private GameObject actual_food;
     private GameObject floor;
+    private GameObject Snake;
+    private GameObject Tail;
+    private int length=0;
     // Start is called before the first frame update
     void Start()
     {
         floor = GameObject.Find("Grid");
         actual_food = GameObject.Find("Food");
+        Snake = GameObject.Find("SnakeInner");
+        Tail = GameObject.Find("Tail");
     }
 
     // Update is called once per frame
@@ -25,6 +35,50 @@ public class Food : MonoBehaviour
             (float)Mathf.Round((float)Random.Range(-26, 12)) + (float)0.5, 
             (float)Mathf.Round((float)Random.Range(-31, 7)) + (float)0.5);
         Debug.Log(actual_food.transform.position);
+
+        // Body piece = obj.AddComponent<Body>();
+        // piece.Body1(sprite11,sprite22,spriteRender12);
+        // piece.gameObject.SetActive(true);
+        // piece.spriteRender.sortingOrder = 40;
+        //myBody[length] = piece;
+        
+             
+        GameObject go1 = new GameObject();
+        go1.name = "go"+length.ToString();
+        go1.AddComponent<SpriteRenderer>();
+
+        go1.transform.SetParent(Snake.transform);
+
+        SpriteRenderer render = go1.GetComponent<SpriteRenderer>();
+        render.sprite = sprite11;
+        go1.transform.position = new Vector2(
+            (float)Mathf.Round((float)Tail.transform.position.x), 
+            (float)Mathf.Round((float)Tail.transform.position.y));
+
+
+            if (Tail.transform.eulerAngles.z == 0.0f){
+                Tail.transform.position = new Vector2(
+            (float)Mathf.Round((float)Tail.transform.position.x-1)+ (float)0.5, 
+            (float)Mathf.Round((float)Tail.transform.position.y)+ (float)0.5);
+            }
+            if (Tail.transform.eulerAngles.z == 0.90f){
+                Tail.transform.position = new Vector2(
+                    (float)Mathf.Round((float)Tail.transform.position.x)+ (float)0.5,
+                    (float)Mathf.Round((float)Tail.transform.position.y-1)+ (float)0.5);
+            }
+            if (Tail.transform.eulerAngles.z == 0.180f){
+                Tail.transform.position = new Vector2(
+            (float)Mathf.Round((float)Tail.transform.position.x+1)+ (float)0.5, 
+            (float)Mathf.Round((float)Tail.transform.position.y)+ (float)0.5);
+            }
+            if (Tail.transform.eulerAngles.z == 0.270f){
+                Tail.transform.position = new Vector2(
+            (float)Mathf.Round((float)Tail.transform.position.x)+ (float)0.5, 
+            (float)Mathf.Round((float)Tail.transform.position.y+1)+ (float)0.5);
+            }
+        length++;
+        //myBody[length] = go1;
+        
     }
 
     public static Rect Get_Rect(GameObject gameObject)
